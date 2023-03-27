3d7f2a10b52fd757fb0a4b6857c0e7ebf8d90508
caroline portela <carol10.portela12@gmail.com>
2023-03-27 11:19:15 -0300

    começando o js

---
+++ js/home.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import { carregarCards } from "./ApiHome.js"
+
+const cards = await carregarCards();
+
+const criarCards = (card) => {
+    const cardsCursos = document.createElement('div')
+    cardsCursos.classList.add('container-cards-cursos')
+
+    const containerCurso = document.createElement('div')
+    containerCurso.classList.add('curso-container')
+    
+    const tipoCurso = document.createElement ('div')
+    tipoCurso.classList.add('titulo-do-curso')
+
+    const iconesDosCursos = document.createElement('i')
+    iconesDosCursos.src = `./${card.icone}`
+
+    const nomeDoCurso = document.createElement ('h1')
+    nomeDoCurso.classList.add('tipo-curso')
+
+    nomeDoCurso.textContent = card.nome
+
+    cardsCursos.append(containerCurso)
+    containerCurso.append(tipoCurso)
+    tipoCurso.append(iconesDosCursos,nomeDoCurso)
+
+    return cardsCursos
+}
+
+ const carregarCards = () => {
+    const elementosDoContainer = document.getElementById('container-cards-cursos')
+    const cursos = cards.map(criarCards)
+
+    elementosDoContainer.replaceChildren(...cursos)
+ }
+
+ carregarCards()
